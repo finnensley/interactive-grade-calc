64381bd1a262305e537b934fbd424d44eb1955b5
finnensley <finniganensley@gmail.com>
2025-06-10 09:05:05 -0600

    added calculateAverage and assignGrade functions to script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// need a function that returns the average
+
+function calculateAverage(score1, score2, score3) {
+    return (score1 + score2 + score3)/ 3
+}
+// need a function to assign the grade
+
+function assignGrade(average) {
+    if (average >= 90) {
+        return "A";
+    } else if (average >= 80) {
+        return "B";
+    } else if (average >= 70) {
+        return "C";
+    } else if (average >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }}
+
+// get the user input, prompt
+
+
+// display the results, console.log
